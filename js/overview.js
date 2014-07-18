faaed00d2df0aaac0ad5b13cbd0326f1de047649
root <root@paresh-Lenovo-G480>
2014-07-18 11:48:49 +0530

    Delete old files

--- js/overview.js
+++
@@ -1,9 +0,0 @@
-(function($){
-	$('#wpbody .wrap').wrapInner('<div id="cptm-col-left" />');
-	$('#wpbody .wrap').wrapInner('<div id="cptm-cols" />');
-	$('#cptm-col-right').removeClass('hidden').prependTo('#cptm-cols');
-	$('#cptm-cpt-overview').removeClass('hidden').insertBefore('#cptm-col-left #ajax-response');
-
-	$('#cptm-col-left > .icon32').insertBefore('#cptm-cols');
-	$('#cptm-col-left > h2').insertBefore('#cptm-cols');
-})(jQuery);
