24cbedfcb1f902dafe71eca2e3de70b3e2c813c2
root <root@paresh-Lenovo-G480>
2014-07-20 16:14:36 +0530

    delete added

--- cpt.php
+++ cpt.php
@@ -25,7 +25,16 @@ class CptSettingsPage {
 
         add_action('admin_menu', array($this, 'add_plugin_page')); //Add menu inside setting for Generator
         add_action('admin_init', array($this, 'page_init')); // Set setting page for CPT Generator
-        add_action('init', array($this, 'register_cpt'));//Register all CPT added using this plugin
+        
+        /*
+         * delete cpt
+         */
+        if (isset($_GET["editmode"]) && $_GET["editmode"]=="delete") {
+             unset($this->options[$_GET["post_type"]]);
+             update_option("cpt_option", $this->options);
+        }
+        
+        add_action('init', array($this, 'register_cpt')); //Register all CPT added using this plugin
     }
 
     function register_cpt() {
@@ -79,33 +88,33 @@ class CptSettingsPage {
 
             </form>
         </div>
- <?php if ($this->options) { ?>
-<table class="wp-list-table widefat fixed pages">
-    <thead>
-    <th class="manage-column">Post Name</th>
-    <th class="manage-column">Public</th>
-    <th class="manage-column">Label</th>
-    </thead>
-    <tbody>
-       <?php  foreach ($this->options as $value) { ?>
-        <tr>
-            <td class="post-title page-title column-title">
-                <strong><a><?php echo $value['post_type']; ?></a></strong>
-                <div class="row-actions">
-                    <span class="edit"><a href="" title="Edit this item">Edit</a> | </span>
-                    <span class="inline hide-if-no-js"><a href="#" class="editinline" title="Edit this item inline">Quick&nbsp;Edit</a> | </span>
-                    <span class="trash"><a class="submitdelete" title="Move this item to the Trash" href="">Trash</a> | </span>
-                    <span class="view"><a href="" title="View “Hello world!”" rel="permalink">View</a></span>
-                </div>
-            </td>
-            <td><?php echo $value['public']; ?></td>
-            <td><?php echo $value['labels_name']; ?></td>
-        </tr>
-       <?php } ?>
-    </tbody>
-</table>
-        <?php
- }
+        <?php if ($this->options) { ?>
+            <table class="wp-list-table widefat fixed pages">
+                <thead>
+                <th class="manage-column">Post Name</th>
+                <th class="manage-column">Public</th>
+                <th class="manage-column">Label</th>
+            </thead>
+            <tbody>
+                <?php foreach ($this->options as $value) { ?>
+                    <tr>
+                        <td class="post-title page-title column-title">
+                            <strong><a><?php echo $value['post_type']; ?></a></strong>
+                            <div class="row-actions">
+                                <span class="edit"><a href="" title="Edit this item">Edit</a> | </span>
+                                <span class="inline hide-if-no-js"><a href="#" class="editinline" title="Edit this item inline">Quick&nbsp;Edit</a> | </span>
+                                <span class="trash"><a class="submitdelete" href="http://child-theme-test.com/wp-admin/options-general.php?page=cpt-generator&editmode=delete&post_type=<?php echo $value['post_type']; ?>" title="Move this item to the Trash" href="">Trash</a> | </span>
+                                <span class="view"><a href="" title="View “Hello world!”" rel="permalink">View</a></span>
+                            </div>
+                        </td>
+                        <td><?php echo $value['public']; ?></td>
+                        <td><?php echo $value['labels_name']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+            </table>
+            <?php
+        }
     }
 
     /**
@@ -182,36 +191,23 @@ class CptSettingsPage {
     public function sanitize($input) {
 
         $cpt_option = get_option('cpt_option'); // Get the current options from the db (Edit 
-// this and return the full modified version)
+
         $editmode = $_POST['editmode'];  // add, edit or delete     
-        $cpt_amount = count($cpt_option);
-        if (get_option('cpt_option')) {
-            $cpt_amount++;
-        }
-        
+
         if ($editmode == 'add') {
-
 // Do Add Logic
-            $cpt_option[$cpt_amount]["post_type"] = $_POST["post_type"];
-            $cpt_option[$cpt_amount]["labels_name"] = isset($_POST["labels_name"]) ? $_POST["labels_name"] : "";
-            $cpt_option[$cpt_amount]["labels_singular_name"] = isset($_POST["labels_singular_name"]) ? $_POST["labels_singular_name"] : "";
-            $cpt_option[$cpt_amount]["public"] = isset($_POST["public"]) ? true : false;
-            $cpt_option[$cpt_amount]["has_archive"] = isset($_POST["has_archive"]) ? true : false;
-            $cpt_option[$cpt_amount]["supports"] = isset($_POST["supports"]) ? $_POST["supports"] : array('');
-
-//            var_dump($cpt_option);
-//            die();
-
+            $cpt_option[$_POST["post_type"]]["post_type"] = $_POST["post_type"];
+            $cpt_option[$_POST["post_type"]]["labels_name"] = $_POST["labels_name"];
+            $cpt_option[$_POST["post_type"]]["labels_singular_name"] = isset($_POST["labels_singular_name"]) ? $_POST["labels_singular_name"] : $_POST["post_type"];
+            $cpt_option[$_POST["post_type"]]["public"] = isset($_POST["public"]) ? true : false;
+            $cpt_option[$_POST["post_type"]]["has_archive"] = isset($_POST["has_archive"]) ? true : false;
+            $cpt_option[$_POST["post_type"]]["supports"] = isset($_POST["supports"]) ? $_POST["supports"] : array('');
             return $cpt_option;
         } elseif ($editmode == 'edit') {
-
 // Do Edit Logic
-
             return $cpt_option;
         } elseif ($editmode == 'delete') {
-
 // Do Delete Logic
-
             return $cpt_option;
         }
 
@@ -239,7 +235,7 @@ class CptSettingsPage {
      */
     public function labels_name_callback() {
         ?>
-        <input type="text" name="labels_name" />
+        <input type="text" name="labels_name" required=""/>
         <?php
     }
 
