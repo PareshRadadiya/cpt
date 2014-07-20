d16745f6de437bd5f6848557a9c6572993c035ee
root <root@paresh-Lenovo-G480>
2014-07-20 19:59:59 +0530

    small case post_type edit fix

--- cpt.php
+++ cpt.php
@@ -14,7 +14,7 @@ class CptSettingsPage {
     /**
      * Holds the values to be used in the fields callbacks
      */
-    private $options, $dir,$editval;
+    private $options, $dir, $editval;
 
     /**
      * Start up
@@ -25,15 +25,15 @@ class CptSettingsPage {
         $this->editval;
         add_action('admin_menu', array($this, 'add_plugin_page')); //Add menu inside setting for Generator
         add_action('admin_init', array($this, 'page_init')); // Set setting page for CPT Generator
-      
+
         /*
          * delete cpt
          */
         if (isset($_GET["editmode"]) && $_GET["editmode"] == "delete") {
-            unset($this->options[$_GET["post_type"]]);
+            unset($this->options[$_GET["cpt_post_type"]]);
             update_option("cpt_option", $this->options);
         } elseif (isset($_GET["editmode"]) && $_GET["editmode"] == "edit") {
-            $this->editval = $this->options[$_GET["post_type"]];
+            $this->editval = $this->options[$_GET["cpt_post_type"]];
         }
 
         add_action('init', array($this, 'register_cpt')); //Register all CPT added using this plugin
@@ -42,14 +42,14 @@ class CptSettingsPage {
     function register_cpt() {
         if ($this->options) {
             foreach ($this->options as $value) {
-                register_post_type($value['post_type'], array(
+                register_post_type($value['cpt_post_type'], array(
                     'labels' => array(
                         'name' => $value['labels_name'],
                         'singular_name' => $value['labels_singular_name']
                     ),
                     'public' => $value['public'],
                     'has_archive' => $value['has_archive'],
-                    'rewrite' => array('slug' => $value['post_type']),
+                    'rewrite' => array('slug' => $value['cpt_post_type']),
                     'supports' => $value['supports']
                         )
                 );
@@ -65,7 +65,6 @@ class CptSettingsPage {
         add_options_page(
                 'CPT Generator', 'CPT Generator', 'manage_options', 'cpt-generator', array($this, 'create_admin_page')
         );
-        
     }
 
     /**
@@ -73,7 +72,7 @@ class CptSettingsPage {
      */
     public function create_admin_page() {
 // Set class property
-        
+
         $this->options = get_option('cpt_option');
         wp_register_style('cpt_generator_style', $this->dir . '/css/switch.css');
         wp_enqueue_style('cpt_generator_style');
@@ -102,15 +101,15 @@ class CptSettingsPage {
                 <?php foreach ($this->options as $value) { ?>
                     <tr>
                         <td class="post-title page-title column-title">
-                            <strong><a><?php echo $value['post_type']; ?></a></strong>
+                            <strong><a><?php echo $value['cpt_post_type']; ?></a></strong>
                             <div class="row-actions">
-                                <span class="edit"><a href="http://child-theme-test.com/wp-admin/options-general.php?page=cpt-generator&editmode=edit&post_type=<?php echo $value['post_type']; ?>" title="Edit this item">Edit</a> | </span>
-                                <span class="inline hide-if-no-js"><a href="#" class="editinline" title="Edit this item inline">Quick&nbsp;Edit</a> | </span>
-                                <span class="trash"><a class="submitdelete" href="http://child-theme-test.com/wp-admin/options-general.php?page=cpt-generator&editmode=delete&post_type=<?php echo $value['post_type']; ?>" title="Move this item to the Trash" href="">Trash</a> | </span>
-                                <span class="view"><a href="" title="View “Hello world!”" rel="permalink">View</a></span>
+                                <span class="edit"><a href="http://child-theme-test.com/wp-admin/options-general.php?page=cpt-generator&editmode=edit&cpt_post_type=<?php echo $value['cpt_post_type']; ?>" title="Edit this item">Edit</a> | </span>
+                                
+                                <span class="trash"><a class="submitdelete" href="http://child-theme-test.com/wp-admin/options-general.php?page=cpt-generator&editmode=delete&cpt_post_type=<?php echo $value['cpt_post_type']; ?>" title="Move this item to the Trash" href="">Trash</a> | </span>
+                              
                             </div>
                         </td>
-                        <td><?php echo $value['public']; ?></td>
+                        <td><?php echo $value['public']?"True":"False"; ?></td>
                         <td><?php echo $value['labels_name']; ?></td>
                     </tr>
                 <?php } ?>
@@ -138,7 +137,7 @@ class CptSettingsPage {
         );
 
         add_settings_field(
-                'post_type', // ID
+                'cpt_post_type', // ID
                 'Post Type', // Title 
                 array($this, 'post_type_callback'), // Callback
                 'cpt-generator', // Page
@@ -184,7 +183,6 @@ class CptSettingsPage {
                 'cpt-generator', // Page
                 'cpt_setting_section' // Section           
         );
-           
     }
 
     /**
@@ -195,12 +193,12 @@ class CptSettingsPage {
     public function sanitize($input) {
         $cpt_option = get_option('cpt_option'); // Get the current options from the db (Edit 
 // Do Add Logic
-        $cpt_option[$_POST["post_type"]]["post_type"] = $_POST["post_type"];
-        $cpt_option[$_POST["post_type"]]["labels_name"] = $_POST["labels_name"];
-        $cpt_option[$_POST["post_type"]]["labels_singular_name"] = isset($_POST["labels_singular_name"]) ? $_POST["labels_singular_name"] : $_POST["post_type"];
-        $cpt_option[$_POST["post_type"]]["public"] = isset($_POST["public"]) ? true : false;
-        $cpt_option[$_POST["post_type"]]["has_archive"] = isset($_POST["has_archive"]) ? true : false;
-        $cpt_option[$_POST["post_type"]]["supports"] = isset($_POST["supports"]) ? $_POST["supports"] : array('');
+        $cpt_option[$_POST["cpt_post_type"]]["cpt_post_type"] = $_POST["cpt_post_type"];
+        $cpt_option[$_POST["cpt_post_type"]]["labels_name"] = $_POST["labels_name"];
+        $cpt_option[$_POST["cpt_post_type"]]["labels_singular_name"] = isset($_POST["labels_singular_name"]) ? $_POST["labels_singular_name"] : $_POST["cpt_post_type"];
+        $cpt_option[$_POST["cpt_post_type"]]["public"] = isset($_POST["public"]) ? true : false;
+        $cpt_option[$_POST["cpt_post_type"]]["has_archive"] = isset($_POST["has_archive"]) ? true : false;
+        $cpt_option[$_POST["cpt_post_type"]]["supports"] = isset($_POST["supports"]) ? $_POST["supports"] : array('');
         return $cpt_option;
     }
 
@@ -216,7 +214,7 @@ class CptSettingsPage {
      */
     public function post_type_callback() {
         ?>
-<input type="text" name="post_type" required="" value="<?php echo isset($this->editval)?$this->editval['post_type']:"" ?>" />
+        <input type="text" name="cpt_post_type" required="" value="<?php echo isset($this->editval) ? $this->editval['cpt_post_type'] : "" ?>" />
         <?php
     }
 
@@ -225,7 +223,7 @@ class CptSettingsPage {
      */
     public function labels_name_callback() {
         ?>
-        <input type="text" name="labels_name" required="" value="<?php echo isset($this->editval)? $this->editval['labels_name'] : "" ?>"/>
+        <input type="text" name="labels_name" required="" value="<?php echo isset($this->editval) ? $this->editval['labels_name'] : "" ?>"/>
         <?php
     }
 
@@ -234,7 +232,7 @@ class CptSettingsPage {
      */
     public function labels_singular_name_callback() {
         ?>
-        <input type="text" name="labels_singular_name"  value="<?php echo isset($this->editval)? $this->editval['labels_singular_name'] : "" ?>"/>
+        <input type="text" name="labels_singular_name"  value="<?php echo isset($this->editval) ? $this->editval['labels_singular_name'] : "" ?>"/>
         <?php
     }
 
