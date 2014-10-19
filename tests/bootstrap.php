523db09c72f85fdd83b30a4af69f1d40abf34147
Paresh Radadiya <paresh.radadiya@rtcamp.com>
2014-10-19 11:37:14 +0530

    Bootstap file autoloader added

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,16 @@
 <?php
 
-require_once('Money.php');
+//require_once('Money.php');
+
+
+function loader($class)
+{
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+
+spl_autoload_register('loader');
+
+//require_once('MoneyTest.php');
